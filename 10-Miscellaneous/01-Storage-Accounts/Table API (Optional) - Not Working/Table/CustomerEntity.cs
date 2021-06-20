19d427bf6776d9d05cb6168b01fa2d35f132d1fd
Atin Gupta <atingupta2005@gmail.com>
2021-06-20 11:25:03 +0530

    -

---
+++ 10-Miscellaneous/01-Storage-Accounts/Table API (Optional) - Not Working/Table/CustomerEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+
+
+namespace Table
+{
+    public class ProductEntity:TableEntity
+    {
+        public ProductEntity() { }
+        public string ProductId { get; set; }
+        public string Stock { get; set; }
+        public string ProductName { get; set; }
+        public string Location { get; set; }
+        public string Quantity { get; set; }
+
+    }
+}
